382857affa464d7f6bfbd3c3a87bd88fe5259397
emmanuelonmeje0-pixel <emmanuelonmeje0@gmail.com>
2026-09-24 00:43:34 -0700

    privacy-page

--- src/views/PrivacyPolicyView.vue
+++ src/views/PrivacyPolicyView.vue
@@ -5,8 +5,99 @@ import PageLayout from '@/components/PageLayout.vue';
 </script>
 <template>
     <PageLayout :showHero="false">
-        <section class="h-screen w-screen flex flex-col justify-center items-center">
-            Privacy Policy
+        <section class="min-h-screen w-screen flex flex-col mt-50 ml-60">
+            <div>
+                <h1 class="text-2xl font-semibold text-gray-900 font-sans">
+                    1. Introduction
+                </h1>
+                <p class="mt-4 text-2xl leading-10 text-gray-600 font-sans">
+                    Welcome to TalkSharp! We are committed to protecting your personal information and<br>
+                    your right to privacy. If you have any questions or concerns about this privacy notice, or<br>
+                    our practices with regards to your personal information, please contact us.
+                </p>
+                <p class="mt-3 text-2xl text-gray-600 font-sans">
+                    This privacy notice describes how we might use your information if you:
+                </p>
+                 
+                    <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">Visit our website</li>
+                    <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">Download and use our mobile application – TalkSharp</li>
+                    <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">Engage with us in other related ways, including any sales, marketing, or events</li>
+                
+                <p class="mt-3 text-2xl text-gray-600 font-sans">
+                    In this privacy notice, if we refer to:
+                </p>
+
+                    <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">“App,” we are referring to our application TalkSharp.</li>
+                    <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">“Services,” we are referring to our App and other related services, including any <br>
+                        sales, marketing, or events.</li>
+
+                <p class="mt-3 text-2xl leading-10 text-gray-600 font-sans">
+                    The purpose of this privacy notice is to explain to you in the clearest way possible what<br>
+                    information we collect, how we use it, and what rights you have in relation to it. If there<br>
+                    are any terms in this privacy notice that you do not agree with, please discontinue use<br>
+                    of our Services immediately.
+                </p>
+
+                <h1 class=" mt-5 text-2xl font-semibold text-gray-900 font-sans">
+                    2. What Information Do We Collect?
+                </h1>
+
+                <p class="mt-3 text-2xl leading-10 text-gray-600 font-sans"><b>Personal information you disclose to us:</b> We collect personal information that you<br>
+                    voluntarily provide to us when you register on the App, express an interest in obtaining<br>
+                    information about us or our products and Services, when you participate in activities on<br>
+                    the App, or otherwise when you contact us.
+                </p>
+
+                <p class="mt-3 text-2xl leading-10 text-gray-600 font-sans">
+                    The personal information that we collect depends on the context of your interactions<br>
+                    with us and the App, the choices you make and the products and features you use. The<br>
+                    personal information we collect may include the following: Name, Email Address,<br>
+                    Phone Number, Password, User Profile Data including learning language, progress,<br>
+                    and other similar information.
+                </p>
+
+                <p class="mt-3 text-2xl leading-10 text-gray-600 font-sans"><b>Information automatically collected:</b>We automatically collect certain information<br>
+                    when you visit, use or navigate the App. This information does not reveal your specific<br>
+                    identity, such as your name or contact information, but may include device and usage<br>
+                    information, such as your IP address, browser and device characteristics, operating<br> system, language preferences, referring URLs, device name, country, location,<br>
+                    information about how and when you use our App and other technical information.
+                </p>
+
+                <h1 class=" mt-5 text-2xl font-semibold text-gray-900 font-sans">
+                    3. How Do We Use Your Information?
+                </h1>
+
+                <p class="mt-3 text-2xl leading-10 text-gray-600 font-sans">
+                    We use personal information collected via our App for a variety of business purposes<br>
+                    described below. We process your personal information for these purposes in reliance<br>
+                    on our legitimate business interests, in order to enter into or perform a contract with<br>
+                    you, with your consent, and/or for compliance with our legal obligations.
+                </p>
+
+                <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">To facilitate account creation and logon process.</li>
+                <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">To post testimonials.</li>
+                <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">Request feedback.</li>
+                <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">To enable user-to-user communications.</li>
+                <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">To manage user accounts.</li>
+                <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">To send administrative information to you.</li>
+                <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">To protect our Services.</li>
+                <li class="mt-2 ml-8 text-2xl text-gray-600 font-sans">To enforce our terms, conditions and policies for business purposes, to comply with<br>
+                    legal and regulatory requirements or in connection with our contract.</li>
+
+                <h1 class=" mt-5 text-2xl font-semibold text-gray-900 font-sans">
+                    4. How Can You Contact Us About This Policy?
+                </h1>
+
+                <p class="mt-3 text-2xl leading-10 text-gray-600 font-sans">
+                    If you have questions or comments about this notice, you may email us at<br>
+                    <a class=" underline" href="mailto:hello@talksharp.co"></a>or by post to:<br>
+                    TalkSharp Inc.<br>
+                    London, UK<br>
+                    United Kingdom
+                </p>
+
+                
+            </div>
         </section>
     </PageLayout>
 </template>
